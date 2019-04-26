8fc1a9215d9e0e74eb86a516c3042dfe5a65fb8b
BernLau <bcl0720@gmail.com>
2019-04-26 13:27:05 -0500

    Added password changing powershell script

---
+++ password_changer.ps1
@@ -0,0 +1,13 @@
+$username_array = @()
+$password_array = @()
+Add-Type -AssemblyName System.Web
+ForEach($name in Get-Content -Path C:\Users\Administrator\Desktop\bernard_testing\usernames.txt) {
+    $password = [System.Web.Security.Membership]::GeneratePassword(24,5)
+    Write-Host $name
+    Write-Host $password
+    $secure_string = (ConvertTo-SecureString -AsPlainText "$password" -Force)
+    $username_array += $name
+    $password_array += $secure_string
+    Set-ADAccountPassword –Identity $name –Reset –NewPassword $secure_string
+    
+}
