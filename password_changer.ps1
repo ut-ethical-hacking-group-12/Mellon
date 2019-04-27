ae456e897da4982659316faacd125bf72d0eec08
BernLau <bcl0720@gmail.com>
2019-04-27 16:00:38 -0500

    changed path to usernames to a relative path

--- password_changer.ps1
+++ password_changer.ps1
@@ -1,13 +1,13 @@
 $username_array = @()
 $password_array = @()
 Add-Type -AssemblyName System.Web
-ForEach($name in Get-Content -Path C:\Users\Administrator\Desktop\bernard_testing\usernames.txt) {
+ForEach($name in Get-Content -Path .\usernames.txt) {
     $password = [System.Web.Security.Membership]::GeneratePassword(24,5)
     Write-Host $name
     Write-Host $password
     $secure_string = (ConvertTo-SecureString -AsPlainText "$password" -Force)
     $username_array += $name
     $password_array += $secure_string
-    Set-ADAccountPassword –Identity $name –Reset –NewPassword $secure_string
+    Set-ADAccountPassword â€“Identity $name â€“Reset â€“NewPassword $secure_string
     
 }
